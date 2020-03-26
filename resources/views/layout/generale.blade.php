508c4ab23292f6e374c7eb2a6f7000f7c658cc3f
LouiseSzczepaniak <50354408+LouiseSzczepaniak@users.noreply.github.com>
2020-03-26 10:26:15 +0100

    début popup

--- resources/views/layout/generale.blade.php
+++ resources/views/layout/generale.blade.php
@@ -14,7 +14,7 @@
             <div class="logo"></div>
         </a>
         @guest
-        <div class="login"></div>
+        <a href="#modal"><div class="login"></div></a>
         @else
             <div class="icons">
                 <a href="/utilisateur/{{Auth::user()->id}}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -35,7 +35,93 @@
 
 <div class="modal">
     @guest
-        <div class="connexion">
+    <div class="remodal" data-remodal-id="modal" role="dialog" aria-labelledby="modal1Title" aria-describedby="modal1Desc">
+  <button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
+  <div>
+    <h2 id="modal1Title">Remodal</h2>
+    <p id="modal1Desc">
+      Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin
+      with declarative state notation and hash tracking.
+    </p>
+  </div>
+  <br>
+  <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
+  <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+</div>
+
+<div data-remodal-id="modal2" role="dialog" aria-labelledby="modal2Title" aria-describedby="modal2Desc">
+  <div>
+    <h2 id="modal2Title">Another one window</h2>
+    <p id="modal2Desc">
+      Hello!
+    </p>
+  </div>
+  <br>
+  <button data-remodal-action="confirm" class="remodal-confirm">Hello!</button>
+</div>
+
+<!-- You can define the global options -->
+<script>
+  // window.REMODAL_GLOBALS = {
+  //   NAMESPACE: 'remodal',
+  //   DEFAULTS: {
+  //     hashTracking: true,
+  //     closeOnConfirm: true,
+  //     closeOnCancel: true,
+  //     closeOnEscape: true,
+  //     closeOnOutsideClick: true,
+  //     modifier: ''
+  //   }
+  // };
+</script>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../libs/jquery/dist/jquery.min.js"><\/script>')</script>
+<script src="../../js/app.js"></script>
+
+<!-- Events -->
+<script>
+  $(document).on('opening', '.remodal', function () {
+    console.log('opening');
+  });
+
+  $(document).on('opened', '.remodal', function () {
+    console.log('opened');
+  });
+
+  $(document).on('closing', '.remodal', function (e) {
+    console.log('closing' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('closed', '.remodal', function (e) {
+    console.log('closed' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('confirmation', '.remodal', function () {
+    console.log('confirmation');
+  });
+
+  $(document).on('cancellation', '.remodal', function () {
+    console.log('cancellation');
+  });
+
+//  Usage:
+//  $(function() {
+//
+//    // In this case the initialization function returns the already created instance
+//    var inst = $('[data-remodal-id=modal]').remodal();
+//
+//    inst.open();
+//    inst.close();
+//    inst.getState();
+//    inst.destroy();
+//  });
+
+  //  The second way to initialize:
+  $('[data-remodal-id=modal2]').remodal({
+    modifier: 'with-red-theme'
+  });
+</script>
             @include('auth.login')
         </div>
         @if(Route::has('register'))
@@ -56,4 +142,5 @@
 </body>
 <script src="/js/jquery.js"></script>
 <script src="/js/divers.js"></script>
+<script src="../../../js/app.js"></script>
 </html>
