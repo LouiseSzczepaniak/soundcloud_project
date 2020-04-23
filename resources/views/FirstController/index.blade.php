024599fcf216b01435d3787e72ef0fc99e676e6c
LydieV <49896819+LydieV@users.noreply.github.com>
2020-04-23 21:50:23 +0200

    Animation page accueil.

--- resources/views/FirstController/index.blade.php
+++ resources/views/FirstController/index.blade.php
@@ -8,14 +8,15 @@
         <p class="banniere_ou"> Ou</p>
         <a href="/chanson/nouvelle" class="nouvelle_chanson"> Uploadez la vôtre </a>
     </div>
-    <h3 class="titre_section"> Les musiques récemment uplodées </h3>
-    <div class="soulignement"></div>
-    <div class="ensemble_musics">
-        @foreach($chansons as $c)
-            @include("FirstController._chansons")
-        @endforeach
+    <div class="scroll-part content is-invert">
+        <h3 class="titre_section content__to-right"> Les musiques récemment uplodées </h3>
+        <div class="soulignement content__to-right"></div>
+        <div class="ensemble_musics content__to-scale">
+            @foreach($chansons as $c)
+                @include("FirstController._chansons")
+            @endforeach
+        </div>
     </div>
-
     <script src="/js/player.js"></script>
 
 @endsection
