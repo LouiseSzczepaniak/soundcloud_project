5adf2c778320607249da49c99bd7284e95139dd4
LydieV <49896819+LydieV@users.noreply.github.com>
2020-04-25 16:41:58 +0200

    Modif chansonsuploadees sur utilisateur.

--- resources/views/FirstController/_chansonsuploadees.blade.php
+++ resources/views/FirstController/_chansonsuploadees.blade.php
@@ -1,27 +1,6 @@
 <div style="display: none;" class="nbListePlaylist" id="{{$c->count()}}"/></div>
 <div class="music">
     <div class="img_music">
-        <div class="music-hover">
-            <div class="contenu-hover">
-                @auth
-                    @if(Auth::user()->jeLike->contains($c->id))
-                        <a data-pjax class="a_like" href="/like/{{$c->id}}"><div class="like"></div></a>
-                    @else
-                        <a data-pjax class="a_like" href="/like/{{$c->id}}"><div class="like_hover"></div></a>
-                    @endif
-                @endauth
-                <a href="#" data-file="{{$c->url}}" data-name="{{$c->nom}}" data-id="{{$c->id}}" class="chanson lienmusic">
-                    <img src="/img/music_play.png" class="img_hover_lien"  alt="play_music"/>
-                </a>
-                @auth
-                    <div id="{{$c->id}}" class="ensemble_listPlaylist">
-                        <input type="button" class="affichPlaylists" id="affichPlaylists{{$c->id}}"/>
-                    </div>
-                @endauth
-
-            </div>
-        </div>
-
         @auth
             <div class="listePlaylists" id="listePlaylists{{$c->id}}">
                 @foreach(Auth::user()->playlists as $p)
@@ -31,11 +10,14 @@
         @endauth
 
     </div>
-
+    <a href="#" data-file="{{$c->url}}" data-name="{{$c->nom}}" data-id="{{$c->id}}" class="chanson lienmusic">
+        play
+    </a>
     <p class="titre_musique"> {{$c->nom}}</p>
     <p class="style_musique"> {{$c->style}}</p>
     <p class="psn_upload"> Uploadée par
         <a href="/utilisateur/{{$c->utilisateur->id}}"> {{$c->utilisateur->name}}</a>
+        le {{ \Carbon\Carbon::parse($c->created_at)->format('d/m/Y') }}
     </p>
 
 </div>
